79bc59bd525f2275844e2b38332cacfca2cedfe6
Rutika Kolhapure <ruchirakolhapure@gmail.com>
2026-02-27 17:33:12 +0530

    Initial

--- app.py
+++ app.py
@@ -1,133 +1,48 @@
 import streamlit as st
-import numpy as np
-import pickle
+import pandas as pd
+import joblib
+import lightgbm as lgb 
+from geopy.distance import geodesic
 
-# -----------------------------------
-# PAGE CONFIG
-# -----------------------------------
-st.set_page_config(
-    page_title="Fraud Detection System",
-    layout="centered"
-)
+model = joblib.load("fraud_detection_model.jb")
+encoder = joblib.load("label_encoder.jb")
 
-st.title("💳 Transaction Fraud Detection System")
+def haversine(lat1, lon1, lat2, lon2):
+    return geodesic((lat1, lon1),(lat2,lon2)).km
 
-st.write(
-    "Enter transaction details to predict whether the transaction is Fraudulent or Legitimate."
-)
+st.title("Fraud Detection System")
+st.write("Enter the Transaction details Below")
 
-# -----------------------------------
-# LOAD TRAINED MODEL
-# -----------------------------------
-#model = pickle.load(open("best_fraud_model_tuned.pkl", "rb"))
-saved_objects = pickle.load(open("best_fraud_model_tuned.pkl", "rb"))
+merchant = st.text_input("Merchant Name")
+category = st.text_input("Category")
+amt = st.number_input("Transaction Amount", min_value=0.0, format="%.2f")
+lat = st.number_input("Latitude",format="%.6f")
+long = st.number_input("Longitude",format="%.6f")
+merch_lat = st.number_input("Merchant Latitude",format="%.6f")
+merch_long = st.number_input("Merchant Longitude",format="%.6f")
+hour = st.slider("Transaction Hour",0,23,12)
+day =st.slider("Transaction Day",1,31,15)
+month = st.slider("Transaction MOnth",1,12,6)
+gender = st.selectbox("Gender",["Male","Female"])
+cc_num = st.text_input("Credit Card number")
 
-model = saved_objects["model"]
+distance = haversine(lat,long,merch_lat,merch_long)
 
+if st.button("Check For Fraud"):
+    if merchant and category and cc_num:
+        input_data = pd.DataFrame([[merchant, category,amt,distance,hour,day,month,gender, cc_num]],
+                                  columns=['merchant','category','amt','distance','hour','day','month','gender','cc_num'])
+        
+        categorical_col = ['merchant','category','gender']
+        for col in categorical_col:
+            try:
+                input_data[col] = encoder[col].transform(input_data[col])
+            except ValueError:
+                input_data[col]=-1
 
-
-# -----------------------------------
-# USER INPUT SECTION
-# -----------------------------------
-
-transaction_type = st.selectbox(
-    "Transaction Type",
-    ["PAYMENT", "TRANSFER", "CASH_OUT", "DEBIT", "CASH_IN"]
-)
-
-amount = st.number_input(
-    "Transaction Amount",
-    min_value=0.0,
-    format="%.2f"
-)
-
-oldbalanceOrg = st.number_input(
-    "Sender Old Balance",
-    min_value=0.0
-)
-
-newbalanceOrig = st.number_input(
-    "Sender New Balance",
-    min_value=0.0
-)
-
-oldbalanceDest = st.number_input(
-    "Receiver Old Balance",
-    min_value=0.0
-)
-
-newbalanceDest = st.number_input(
-    "Receiver New Balance",
-    min_value=0.0
-)
-
-hour = st.slider(
-    "Transaction Hour (0–23)",
-    0, 23
-)
-
-# -----------------------------------
-# FEATURE ENGINEERING
-# -----------------------------------
-
-type_dict = {
-    "PAYMENT": 0,
-    "TRANSFER": 1,
-    "CASH_OUT": 2,
-    "DEBIT": 3,
-    "CASH_IN": 4
-}
-
-type_enc = type_dict[transaction_type]
-
-# engineered features
-log_amount = np.log1p(amount)
-
-is_high_amount = 1 if amount > 200000 else 0
-
-is_night = 1 if 0 <= hour <= 6 else 0
-
-balance_diff_orig = oldbalanceOrg - newbalanceOrig
-balance_diff_dest = newbalanceDest - oldbalanceDest
-
-# step approximation
-step = hour
-
-# -----------------------------------
-# MODEL INPUT (ORDER MATTERS)
-# -----------------------------------
-
-input_data = np.array([[
-    step,
-    amount,
-    log_amount,
-    is_high_amount,
-    hour,
-    is_night,
-    balance_diff_orig,
-    balance_diff_dest,
-    type_enc
-]])
-
-# -----------------------------------
-# PREDICTION
-# -----------------------------------
-
-if st.button("Check Transaction"):
-
-    probability = model.predict_proba(input_data)[0][1]
-
-    # higher fraud threshold (better for imbalanced data)
-    threshold = 0.90
-    prediction = 1 if probability > threshold else 0
-
-    st.subheader("Prediction Result")
-
-    if prediction == 1:
-        st.error("⚠️ Fraudulent Transaction Detected")
+        input_data['cc_num'] = input_data['cc_num'].apply(lambda x:hash(x) % (10 ** 2))
+        prediction = model.predict(input_data)[0]
+        result = "Fraudulant Transaction" if prediction == 1 else " Legitimate Transaction"
+        st.subheader(f"Prediction: {result}")
     else:
-        st.success("✅ Legitimate Transaction")
-
-    st.write(f"Fraud Probability: {probability:.2%}")
-
-    st.progress(float(probability))
+        st.error("Please Fill all required fields")
